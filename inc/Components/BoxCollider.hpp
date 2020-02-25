3a8d24f81f28368f35dc2386234da4e782863ab3
asifZaman0362 <zero362001@gmail.com>
2020-02-25 20:04:25 +0530

    add collision methods that hopefully works

--- inc/Components/BoxCollider.hpp
+++ inc/Components/BoxCollider.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "Collider2D.hpp"
 #include <SFML/Graphics/Rect.hpp>
 #include <cmath>
@@ -8,29 +9,23 @@ namespace ze {
         friend class CollisionManager;
     public:
         BoxCollider(const std::weak_ptr<int> object_id) : Collider2D(std::move(object_id)) {}
+        
         bool IsColliding(Collider2D* other) override {
+            
             bool colliding = false;
             if (other->GetType() == ColliderType::Rectangle) {
                 BoxCollider* other_box = dynamic_cast<BoxCollider*>(other);
                 colliding = this->m_aabb.intersects(other_box->GetRect());
             } else if (other->GetType() == ColliderType::Circle) {
                 CircleCollider* other_circle = dynamic_cast<CircleCollider*>(other);
-                sf::Vector2f circ_pos = other_circle->GetPosition();
-                float sqr_dist_nw = pow(m_aabb.left - circ_pos.x, 2) + pow(m_aabb.top - circ_pos.y, 2);
-                float sqr_dist_ne = pow(m_aabb.left - m_aabb.width - circ_pos.x, 2) + pow(m_aabb.top - circ_pos.y, 2);
-                float sqr_dist_sw = pow(m_aabb.left - circ_pos.x, 2) + pow(m_aabb.top - m_aabb.height - circ_pos.y, 2);
-                float sqr_dist_se = pow(m_aabb.left - m_aabb.width - circ_pos.x, 2) + pow(m_aabb.top - m_aabb.height - circ_pos.y, 2);
-                if (sqr_dist_ne < pow(other_circle->GetRadius(), 2) || sqr_dist_nw < pow(other_circle->GetRadius(), 2) 
-                    || sqr_dist_se < pow(other_circle->GetRadius(), 2) || sqr_dist_sw < pow(other_circle->GetRadius(), 2)
-                    || m_aabb.contains(other_circle->GetPosition())) {
-                    colliding = true;
-                }
+                colliding = other_circle->IsColliding(this);
             }
             if (colliding) {
                 this->m_contacts.push_back(std::make_shared<Collider2D>(other));
                 other->m_contacts.push_back(std::make_shared<Collider2D>(this));
             }
             return colliding;
+
         }
 
         ColliderType GetType() {
@@ -40,6 +35,7 @@ namespace ze {
         sf::FloatRect GetRect() {
             return m_aabb;
         }
+
     private:
         sf::FloatRect m_aabb;
     };
--- inc/Components/CircleCollider.hpp
+++ inc/Components/CircleCollider.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "Collider2D.hpp"
 #include <SFML/System/Vector2.hpp>
 
@@ -6,13 +7,48 @@ namespace ze {
     class CircleCollider : public Collider2D {
     public:
         CircleCollider(const std::weak_ptr<int> object_id) : Collider2D(std::move(object_id)) {}
-        ~CircleCollider() {}
+        
+        bool IsColliding(Collider2D* other) override {
+            if (other->GetType() == ColliderType::Circle) {
+            
+                CircleCollider* other_circle = dynamic_cast<CircleCollider*>(other);
+                auto other_pos = other_circle->GetPosition();
+                float other_radius = other_circle->GetRadius();
+                float dist_sqr = pow(other_pos.x - m_position.x, 2) + pow(other_pos.y - m_position.y, 2);
+                return dist_sqr <= (other_radius + m_radius) * (other_radius + m_radius);
+            
+            } else if (other->GetType() == ColliderType::Rectangle) {
+                
+                BoxCollider* other_box = dynamic_cast<BoxCollider*>(other);
+                auto other_rect = other_box->GetRect();
+                float distx = abs(other_rect.left + other_rect.width/2 - m_position.x);
+                float disty = abs(other_rect.top + other_rect.height/2 - m_position.y);
+                if (distx > other_rect.width/2 + m_radius || disty > other_rect.height/2 + m_radius) { 
+                    return false;
+                } else {
+                    float dist = sqrt(pow(other_rect.left + other_rect.width/2 - m_position.x, 2) +
+                                 pow(other_rect.top + other_rect.height/2 - m_position.y, 2));
+                    float hyp = sqrt(pow(other_rect.width/2, 2) + pow(other_rect.height/2, 2));
+                    if (dist - hyp <= m_radius) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        
+        ColliderType GetType() {
+            return ColliderType::Circle;
+        }
+
         sf::Vector2f GetPosition() {
             return m_position;
         }
+
         float GetRadius() {
             return m_radius;
         }
+
     private:
         sf::Vector2f m_position;
         float m_radius;
